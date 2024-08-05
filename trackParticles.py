dede32134204ddf279379eff8621223843b3a72c
dangom_ya <hayashi.yasuhito.ta@alumni.tsukuba.ac.jp>
2024-08-05 18:56:54 +0900

    Update trackParticles.py
    numba_高速化

--- trackParticles.py
+++ trackParticles.py
@@ -3,6 +3,7 @@ import numpy as np
 import pickle
 from collections import deque
 from tqdm import tqdm
+from numba import njit, jit
 
 # CSVファイルのパス
 file_path = 'suruga_test_short.csv'
@@ -21,26 +22,31 @@ class Particle:
         self.particle_id = particle_id
         self.events = deque([(x, y, time)])  # Events are stored with coordinates and time
         self.mass = 1
-        self.centroid = np.array([x, y])
+        self.centroid = np.array([x, y], dtype=np.float64)
         self.centroid_history = [(time, self.centroid.copy())]
-    
+
     def add_event(self, x, y, time):
         self.events.append((x, y, time))
         self.mass += 1
         # Remove old events outside the 10 ms window
         while self.events and self.events[0][2] < time - 10000:
             self.events.popleft()
-        
+
         # Update centroid
-        coords = np.array([event[:2] for event in self.events])
+        coords = np.array([event[:2] for event in self.events], dtype=np.float64)
         self.centroid = coords.mean(axis=0)
         self.centroid_history.append((time, self.centroid.copy()))
-    
+
     def is_active(self, current_time, m_threshold):
         # A particle is inactive if no events in 2 ms and mass < M
         if self.events and self.events[-1][2] < current_time - 2000:
             return self.mass > m_threshold
         return True
+
+# JIT compile distance calculation
+@njit
+def calculate_distance_sq(x1, y1, x2, y2):
+    return (x1 - x2) ** 2 + (y1 - y2) ** 2
 
 # パーティクル追跡
 def track_particles(data, spatial_radius=24, time_window=2000, m_threshold=10000):
@@ -52,16 +58,16 @@ def track_particles(data, spatial_radius=24, time_window=2000, m_threshold=10000
 
     for event in tqdm(data.itertuples(index=False, name='Event'), total=len(data), desc="Processing events"):
         x, y, time = event.x, event.y, event.time
-        
+
         # Check for nearby active particles
         nearby_particle_id = None
         for particle in active_particles:
             # Calculate spatial distance squared for efficiency
-            if (np.sum((particle.centroid - np.array([x, y])) ** 2) <= spatial_radius_squared and
-                    particle.events[-1][2] >= time - time_window):
+            dist_sq = calculate_distance_sq(particle.centroid[0], particle.centroid[1], x, y)
+            if dist_sq <= spatial_radius_squared and particle.events[-1][2] >= time - time_window:
                 nearby_particle_id = particle.particle_id
                 break
-        
+
         if nearby_particle_id is not None:
             # Update existing particle
             particles[nearby_particle_id].add_event(x, y, time)
@@ -74,7 +80,7 @@ def track_particles(data, spatial_radius=24, time_window=2000, m_threshold=10000
 
         # Clean up inactive particles
         active_particles = [p for p in active_particles if p.is_active(time, m_threshold)]
-    
+
     return particles
 
 # パーティクル追跡の実行
